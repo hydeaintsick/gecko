b657c479c831da4572d789002e667e6de1da330e
Bnktop <bnktop@Bnktops-MacBook-Pro.local>
2025-04-10 21:09:28 +0200

    small ts error fixed

--- hooks/use-init-auth.ts
+++ hooks/use-init-auth.ts
@@ -2,7 +2,6 @@ import { useAuthStore } from "@/lib/store";
 import { useEffect } from "react";
 
 export const useInitAuth = () => {
-  // @ts-expect-error
   const initialize = useAuthStore((state) => state.initialize);
 
   useEffect(() => {
